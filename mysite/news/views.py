f87ad7123787b9b19ca48651fdfb34d7f71fbbcd
DatKorso <sergiofeed@yandex.ru>
2021-09-23 02:04:19 +0300

    Изучил шаблоны и рендеринг + вывод из БД

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import  HttpResponse
 
-def index(request):
-    # print(dir(request))
-    return HttpResponse('Hello world')
+from .models import News
 
-def test(request):
-    return HttpResponse('<h1>Тестовая страница</h1>')
+def index(request):
+    news = News.objects.all()
+    context = {
+        'news': news,
+        'title': 'Список новостей'}
+    return render(request, template_name='news/index.html',context=context)
