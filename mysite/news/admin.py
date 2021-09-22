cf8b9782697bd83efd68d7885a3b566d3aa966d5
DatKorso <sergiofeed@yandex.ru>
2021-09-23 02:30:57 +0300

    Выгрузил приложение в админку и настроил модель для удобного использования

--- mysite/news/admin.py
+++ mysite/news/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import News
+
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ('id','title','created_at','updated_at', 'is_published')
+    list_display_links = ('id', 'title')
+    search_fields = ('id', 'title')
+
+admin.site.register(News, NewsAdmin)
--- mysite/news/models.py
+++ mysite/news/models.py
@@ -2,12 +2,17 @@ from django.db import models
 
 
 class News(models.Model):
-    title = models.CharField(max_length=150)
-    content = models.TextField(blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
-    is_published = models.BooleanField(default=True)
+    title = models.CharField(max_length=150, verbose_name='Наименование')
+    content = models.TextField(blank=True, verbose_name='Содержимое')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Создано')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Обновлено')
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    is_published = models.BooleanField(default=True, verbose_name='Опубликовано')
 
     def __str__(self):
         return self.title
+
+    class Meta:
+        verbose_name ='Новость'
+        verbose_name_plural = 'Новости'
+        ordering = ['-created_at']
