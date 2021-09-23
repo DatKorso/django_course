4303a1058bf33f50862e4cb1bfd0f0bfdef77b4b
DatKorso <sergiofeed@yandex.ru>
2021-09-23 08:55:54 +0300

    Добавил сайдбар и создал динамичные страницы категорий

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
-from django.http import  HttpResponse
 
-from .models import News
+from .models import News, Category
+
 
 def index(request):
     news = News.objects.all()
+    categories = Category.objects.all()
     context = {
         'news': news,
-        'title': 'Список новостей'}
+        'title': 'Список новостей',
+        'categories' : categories,
+    }
     return render(request, template_name='news/index.html',context=context)
+
+def get_category(request, category_id):
+    news = News.objects.filter(category_id=category_id)
+    categories = Category.objects.all()
+    category = Category.objects.get(pk=category_id)
+    return render(request, 'news/category.html', {'news': news, 'categories': categories, 'category':category})
