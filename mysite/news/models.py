d1ce92c073d4c78eabf9340df14397f88786de27
DatKorso <sergiofeed@yandex.ru>
2021-09-23 06:34:36 +0300

    Реализовал связи между таблицами и оформил админку

--- mysite/news/models.py
+++ mysite/news/models.py
@@ -1,12 +1,25 @@
 from django.db import models
 
 
+class Category(models.Model):
+    title = models.CharField(max_length=150, db_index=True, verbose_name='Наименование')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ['title']
+
+
 class News(models.Model):
-    title = models.CharField(max_length=150, verbose_name='Наименование')
+    title = models.CharField(max_length=150, db_index=True, verbose_name='Наименование')
     content = models.TextField(blank=True, verbose_name='Содержимое')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Создано')
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Обновлено')
-    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True, verbose_name='Изображение')
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, verbose_name='Категория', null=True)
     is_published = models.BooleanField(default=True, verbose_name='Опубликовано')
 
     def __str__(self):
