1e1be7bf0556cad228e1085447bf5283d48decab
DatKorso <sergiofeed@yandex.ru>
2021-09-23 11:31:23 +0300

    Создал пользовательские теги шаблона (simple_tag, inclusion_tag)

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -5,16 +5,13 @@ from .models import News, Category
 
 def index(request):
     news = News.objects.all()
-    categories = Category.objects.all()
     context = {
         'news': news,
         'title': 'Список новостей',
-        'categories' : categories,
     }
     return render(request, template_name='news/index.html',context=context)
 
 def get_category(request, category_id):
     news = News.objects.filter(category_id=category_id)
-    categories = Category.objects.all()
     category = Category.objects.get(pk=category_id)
-    return render(request, 'news/category.html', {'news': news, 'categories': categories, 'category':category})
+    return render(request, 'news/category.html', {'news': news, 'category': category})
