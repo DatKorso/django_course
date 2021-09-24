defe9f01a78868ad99e598b9461691c033aa9c1c
DatKorso <sergiofeed@yandex.ru>
2021-09-24 08:29:52 +0300

    Оформил форму и реализовал добавление в бд через post запрос

--- mysite/news/forms.py
+++ mysite/news/forms.py
@@ -2,6 +2,11 @@ from django import forms
 from .models import Category
 
 class NewsForm(forms.Form):
-    title = forms.CharField(max_length=150)
-    content = forms.CharField()
-    category = forms.ModelChoiceField(queryset=Category.objects.all())
+    title = forms.CharField(max_length=150, label='Заголовок', widget=forms.TextInput(attrs={"class": "form-control"}))
+    content = forms.CharField(required=False, label='Содержимое', widget=forms.Textarea(attrs={
+        "class": "form-control",
+        "rows": 5,
+    }))
+    category = forms.ModelChoiceField(queryset=Category.objects.all(), empty_label='Выберите категорию',
+                                      label='Категория', widget=forms.Select(
+        attrs={"class": "form-control"}))
