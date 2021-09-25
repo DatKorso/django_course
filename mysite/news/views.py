1e084551367724872221d34c190153a8ffedea37
DatKorso <sergiofeed@yandex.ru>
2021-09-26 02:20:25 +0300

    Дополнил знания о ListView и реорганизовал категории

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -9,6 +9,7 @@ class HomeNews(ListView):
     model = News
     template_name = 'news/home_news_list.html'
     context_object_name = 'news'
+    allow_empty = False
     #extra_context = {'title': 'Главная'}
 
     def get_context_data(self, *, object_list=None, **kwargs):
@@ -19,13 +20,26 @@ class HomeNews(ListView):
     def get_queryset(self):
         return News.objects.filter(is_published=True)
 
-def index(request):
-    news = News.objects.all()
-    context = {
-        'news': news,
-        'title': 'Список новостей',
-    }
-    return render(request, template_name='news/index.html',context=context)
+class NewsByCategory(ListView):
+    model = News
+    template_name = 'news/home_news_list.html'
+    context_object_name = 'news'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = Category.objects.get(pk=self.kwargs['category_id'])
+        return context
+
+    def get_queryset(self):
+        return News.objects.filter(category_id = self.kwargs['category_id'], is_published=True)
+
+# def index(request):
+#     news = News.objects.all()
+#     context = {
+#         'news': news,
+#         'title': 'Список новостей',
+#     }
+#     return render(request, template_name='news/index.html',context=context)
 
 def get_category(request, category_id):
     news = News.objects.filter(category_id=category_id)
