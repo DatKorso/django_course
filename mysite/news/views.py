eee4cd9014911399301c831dfa289ba85864309d
DatKorso <sergiofeed@yandex.ru>
2021-09-28 17:52:51 +0300

    Класс CreateView Реализовал создание новости при помощи класса CreateView

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
+from django.urls import reverse_lazy
 
 from .models import News, Category
 from .forms import NewsForm
@@ -59,14 +60,21 @@ class ViewNews(DetailView):
 #     news_item = get_object_or_404(News,pk=news_id)
 #     return render(request, 'news/view_news.html', {"news_item": news_item})
 
-def add_news(request):
-    if request.method == 'POST':
-        form = NewsForm(request.POST)
-        if form.is_valid():
-            #print(form.cleaned_data)
-            #news = News.objects.create(**form.cleaned_data) - с моделями делать не нужно
-            news = form.save()
-            return redirect(news)
-    else:
-        form = NewsForm()
-    return render(request,'news/add_news.html', {'form': form})
+class CreateNews(CreateView):
+    form_class = NewsForm
+    template_name = 'news/add_news.html'
+    # На случай если нет get_absolute_url ↓
+    # success_url = reverse_lazy('home')
+
+
+# def add_news(request):
+#     if request.method == 'POST':
+#         form = NewsForm(request.POST)
+#         if form.is_valid():
+#             #print(form.cleaned_data)
+#             #news = News.objects.create(**form.cleaned_data) - с моделями делать не нужно
+#             news = form.save()
+#             return redirect(news)
+#     else:
+#         form = NewsForm()
+#     return render(request,'news/add_news.html', {'form': form})
